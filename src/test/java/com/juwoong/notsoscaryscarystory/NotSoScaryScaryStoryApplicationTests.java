a1042c748b212639c898fab79e86aaab8faf1f88
Juwoong <wwong7247@gmail.com>
2023-07-13 12:27:22 +0900

    프로젝트 초기설정  타임리프, JPA, MYSQL

---
+++ src/test/java/com/juwoong/notsoscaryscarystory/NotSoScaryScaryStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwoong.notsoscaryscarystory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotSoScaryScaryStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
